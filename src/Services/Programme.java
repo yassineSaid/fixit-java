d1e498837cc70986da4e5f8a826e3791d8bd477d
yassine said <yassine.said@esprit.tn>
2019-03-28 11:30:10 +0100

    Update

--- src/Services/Programme.java
+++ src/Services/Programme.java
@@ -1,9 +1,31 @@
 package Services;
 
-public class Programme {
+import java.io.IOException;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Programme extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		try
+		{
+			Parent root = FXMLLoader.load(getClass().getResource("/Gui/Main.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+		}
+		catch (IOException ex)
+		{
+			
+		}
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		launch(args);
 	}
 
 }
