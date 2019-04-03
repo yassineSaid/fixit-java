61421523a7e34826a6e7b67b692a807a4f788a56
slim ben salah <slim.bensalah@esprit.tn>
2019-04-03 17:15:28 +0100

    service with interface

--- src/Gui/BackIndexController.java
+++ src/Gui/BackIndexController.java
@@ -5,9 +5,21 @@
  */
 package Gui;
 
+import Entities.User;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -23,5 +35,34 @@ public class BackIndexController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+      @FXML
+    private Button service;
+
+    @FXML
+    void espaceService(ActionEvent event) {
+              try {
+			
+		 			 
+		 	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Gui/ajouterCategorieService.fxml"));   
+		 	Parent Rec = fxmlLoader.load();          
+		 	AjouterCategorieServiceController controller = fxmlLoader.<AjouterCategorieServiceController>getController();
+		 	controller.setUser(this.getUser());
+                        Scene scene = new Scene(Rec);
+           
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.show();
+            stage.setScene(scene);
+            
+        } catch (IOException ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+private User user;
+    public User getUser() {
+		return user;
+	}
+    public void setUser(User user) {
+		this.user = user;
+	}
     
 }
