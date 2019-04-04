0f83415ecf6880d55b99f2c0029127900eddf981
Mustapha <mustapha.benhajminyaoui@esprit.tn>
2019-04-04 11:10:33 +0100

    javaFx rating control

--- src/Gui/AvisFrontController.java
+++ src/Gui/AvisFrontController.java
@@ -60,6 +60,7 @@ public class AvisFrontController implements Initializable {
             AvisService avisSer = new AvisService();
             avis = avisSer.getUserAvis(this.getUser().getId());
             System.out.println(avis.getId());
+            frontIndexController.getEspaceAvis().setStyle("-fx-background-color: #f4f4f4");
             if (avis == null) {
                 ObservableList<String> list = FXCollections.observableArrayList("Non Satisfait", "Moyennement Satisfait", "Totalement Satisfait");
                 satisfaction.setItems(list);
