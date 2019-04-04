90a2a5cad3a9efd37ae134799247ece8f6c13f97
yassine said <yassine.said@esprit.tn>
2019-04-04 11:29:15 +0100

    Login with keypress

--- src/Gui/LoginController.java
+++ src/Gui/LoginController.java
@@ -1,4 +1,4 @@
- /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -23,6 +23,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 /**
@@ -46,53 +48,55 @@ public class LoginController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-                        errot.setVisible(false);
+        errot.setVisible(false);
 
-    }    
+    }
 
     @FXML
     private void connexionAction(ActionEvent event) {
-                errot.setVisible(false);
-		UserService us=new UserService();
-		System.out.println();
-		if (us.checkUser(login.getText(), password.getText()))
-		{
-			 try {
-				 	User U=us.connect(login.getText());
-                                        if(U.getRoles()=="admin")
-                                        {
-                                            
-				 	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Gui/backIndex.fxml"));   
-				 	Parent back = fxmlLoader.load();
-                                        Scene scene = new Scene(back);
-	               
-	                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-	                stage.show();
-	                stage.setScene(scene);
-                                        }
-                                        else {
-				 	//System.out.println(U.getRoles());
-				 	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Gui/frontIndex.fxml"));   
-				 	Parent front = fxmlLoader.load();          
-				 	FrontIndexController controller = fxmlLoader.<FrontIndexController>getController();
-				 	controller.setUser(U);Scene scene = new Scene(front);
-	               
-	                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-	                stage.show();
-	                stage.setScene(scene);
-                                        }
-				 	
-	                
-	                
-	            } catch (IOException ex) {
-	                Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
-	            }
-		}
-		else
-		{
-                        errot.setText("Verifier vos données");
-			errot.setVisible(true); 
-		}
+        errot.setVisible(false);
+        UserService us = new UserService();
+        System.out.println();
+        if (us.checkUser(login.getText(), password.getText())) {
+            try {
+                User U = us.connect(login.getText());
+                if (U.getRoles() == "admin") {
+
+                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Gui/backIndex.fxml"));
+                    Parent back = fxmlLoader.load();
+                    Scene scene = new Scene(back);
+
+                    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    stage.show();
+                    stage.setScene(scene);
+                } else {
+                    //System.out.println(U.getRoles());
+                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Gui/frontIndex.fxml"));
+                    Parent front = fxmlLoader.load();
+                    FrontIndexController controller = fxmlLoader.<FrontIndexController>getController();
+                    controller.setUser(U);
+                    Scene scene = new Scene(front);
+
+                    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    stage.show();
+                    stage.setScene(scene);
+                }
+
+            } catch (IOException ex) {
+                Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else {
+            errot.setText("Verifier vos données");
+            errot.setVisible(true);
+        }
     }
-    
+
+    @FXML
+    private void keyPressed(KeyEvent event) {
+        if (event.getCode().equals(KeyCode.ENTER)) {
+            ActionEvent ae=new ActionEvent(event.getSource(),event.getTarget());
+            connexionAction(ae);
+        }
+    }
+
 }
