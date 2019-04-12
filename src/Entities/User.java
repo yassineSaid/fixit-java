a953d4e0e254b93da7b72ad043c23f9649fb6d28
Wassim SLATNIA <wassim.slatnia@esprit.tn>
2019-04-12 18:50:00 +0100

    outil

--- src/Entities/User.java
+++ src/Entities/User.java
@@ -87,51 +87,7 @@ public class User {
         this.solde = solde;
     }
 
-	private int id,solde;
-	private String email,username,firstname,lastname,roles,image;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getFirstname() {
-		return firstname;
-	}
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
-	public String getLastname() {
-		return lastname;
-	}
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
-	public String getRoles() {
-		return roles;
-	}
-	public void setRoles(String role) {
-		this.roles = role;
-	}
-        public String getImage() {
-            return image;
-        }
-
-        public void setImage(String image) {
-            this.image = image;
-        }
+	
         
 
         @Override
@@ -198,9 +154,6 @@ public class User {
     
     
 
-    @Override
-    public String toString() {
-        return username;
-    }
+    
 
 }
